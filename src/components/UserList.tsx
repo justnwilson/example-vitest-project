203477ece81b50fde29a8765452227c0168946fe
Justin <wilson.s.justin@gmail.com>
2025-04-02 15:37:00 -0600

    first-changes

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,77 +1,19 @@
-import useUsers from "../hooks/useUsers";
-import userService, { User } from "../services/user-service";
+import React from "react";
 
-const UserList = () => {
-  const { users, error, isLoading, setUsers, setError } = useUsers();
+interface UserListProps {
+  users: { id: number; name: string }[];
+}
 
-  const deleteUser = (user: User) => {
-    const originalUsers = [...users];
-    setUsers(users.filter((u) => u.id !== user.id));
-
-    userService.delete(user.id).catch((err) => {
-      setError(err.message);
-      setUsers(originalUsers);
-    });
-  };
-
-  const addUser = () => {
-    const originalUsers = [...users];
-    const newUser = { id: 0, name: "Mosh" };
-    setUsers([newUser, ...users]);
-
-    userService
-      .create(newUser)
-      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
-      .catch((err) => {
-        setError(err.message);
-        setUsers(originalUsers);
-      });
-  };
-
-  const updateUser = (user: User) => {
-    const originalUsers = [...users];
-    const updatedUser = { ...user, name: user.name + "!" };
-    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
-
-    userService.update(updatedUser).catch((err) => {
-      setError(err.message);
-      setUsers(originalUsers);
-    });
-  };
+const UserList: React.FC<UserListProps> = ({ users }) => {
+  const defaultUsers = [{ id: 1, name: "John Doe" }, { id: 2, name: "Jane Doe" }];
 
   return (
-    <>
-      {error && <p className="text-danger">{error}</p>}
-      {isLoading && <div className="spinner-border"></div>}
-      <button className="btn btn-primary mb-3" onClick={addUser}>
-        Add
-      </button>
-      <ul className="list-group">
-        {users.map((user) => (
-          <li
-            key={user.id}
-            className="list-group-item d-flex justify-content-between"
-          >
-            {user.name}
-            <div>
-              <button
-                className="btn btn-outline-secondary mx-1"
-                onClick={() => updateUser(user)}
-              >
-                Update
-              </button>
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => deleteUser(user)}
-              >
-                Delete
-              </button>
-            </div>
-          </li>
+    <div>
+        {defaultUsers.map((user: { id: React.Key | null | undefined; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
+            <div key={user.id}>{user.name}</div>
         ))}
-      </ul>
-    </>
-  );
+    </div>
+);
 };
 
 export default UserList;
